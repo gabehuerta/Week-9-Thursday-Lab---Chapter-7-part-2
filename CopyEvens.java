8e3acddd52bfd928e01ab615884903c603844a7b
gabehuerta <54081519+gabehuerta@users.noreply.github.com>
2019-12-03 00:39:29 -0600

    Add files via upload

---
+++ CopyEvens.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class CopyEvens
+{
+	public static void main(String[] args){
+		int[] evens = copyEvens(new int[]{4,5,3,6,7,9,8,1,2,5,7,3,9});
+		System.out.println("Array is: 4,5,3,6,7,9,8,1,2,5,7,3,9");
+		System.out.println("Evens array: " + Arrays.toString(evens));
+	}
+	public static int[] copyEvens(int[] a){
+		int evenCount = countEvens(a);
+		int i = 0;
+		int[] evens = new int[evenCount];
+		for(int n : a)
+		{
+			if(n % 2 == 0){
+				evens[i++] = n;
+			}
+		}
+		return evens;
+	}
+	private static int countEvens(int[] a){
+		int count = 0;
+		for(int n : a)
+		{
+			if(n % 2 == 0){
+				count++;
+			}
+		}
+		return count;
+	}	
+}
+
+    
+
+
---
+++ Reverser2.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Reverser2
+{
+	public static int[] reverse(int[] a)
+	{
+		int[] newArray = new int[a.length];
+		for (int i = 0; i < a.length; i++)
+			newArray[(a.length-1) - i] = a[i]; 
+		return newArray;
+	}
+	
+	public static void reverse2(int[] a){
+        	int rev[] = reverse(a); 
+        	for(int i=0; i<rev.length; i++){   
+           		a[i] = rev[i];  
+		}
+	}
+	
+	private static void display(String header, int[] a)
+	{
+		if (header.length() != 0)
+			System.out.print(header + " ");
+		for (int i = 0; i < a.length; i++)
+			System.out.print(a[i] + " ");
+		System.out.println();
+		System.out.println();
+	}
+	
+    	public static void main(String[] args)
+	{		
+		int[] example = { 0, 1, 2, 3, 4, 5 };
+		display("Before reversing:", example);
+		example = reverse(example); 
+		display("After reversing: ", example);
+		
+		int[] example1 = { 1, 2, 3, 4, 5, 6 };
+        	display("Before reversing:", example1);
+        	reverse2(example1);
+		display("After reversing: ", example1);
+	}
+}
+
+    
+
+
